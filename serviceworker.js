70514806756fa77c21f76c6c7a79abc05e622290
s0lanum-tuber0sum <itirmkitchen@gmail.com>
2023-03-09 07:15:15 +0530

    updated

--- serviceworker.js
+++ serviceworker.js
@@ -22,7 +22,7 @@ self.addEventListener('activate', function (event) {
   event.waitUntil(
       caches.keys().then(function(keys){
           return Promise.all(keys.map(function(key, i){
-              if(key !== CACHE_VERSION){
+              if(key !== staticCacheName){
                   return caches.delete(keys[i]);
               }
           }))
