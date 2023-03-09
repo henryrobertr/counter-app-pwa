96e06d0a654066f27fa490f5ef588ac6c8b8ca96
s0lanum-tuber0sum <itirmkitchen@gmail.com>
2023-03-09 20:15:13 +0530

    updated version controll

--- serviceworker.js
+++ serviceworker.js
@@ -1,46 +1,75 @@
-var staticCacheName = "counter-pwa";
+/* https://github.com/MicrosoftEdge/Demos/blob/main/pwamp/sw.js */
+
+const VERSION = "v333";
+const CACHE_NAME = `counterapp-${VERSION}`;
  
+// Those are all the resources our app needs to work.
+// We'll cache them on install.
 const INITIAL_CACHED_RESOURCES = [
-  '/',
-  './styles.css',
-  'https://unpkg.com/react@18/umd/react.production.min.js',
-  'https://unpkg.com/react-dom@18.2.0/umd/react-dom.production.min.js',
-  'https://unpkg.com/babel-standalone@6/babel.min.js',
-  './App.js',
+  './',
   './images/favicon.ico',
   './images/icon-192x192.png',
   './images/icon-512x512.png',
   './App.js',
+  './index.html',
+  './styles.css',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18.2.0/umd/react-dom.production.min.js',
+  'https://unpkg.com/babel-standalone@6/babel.min.js',
 ];
 
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(INITIAL_CACHED_RESOURCES);
-    })
-  );
-});
- 
-self.addEventListener("fetch", function (event) {
-  console.log(event.request.url);
-  console.log(caches);
-  let online = navigator.onLine;
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      console.log(response)
-      return response || fetch(event.request);
-    })
-  );
+// Add a cache-busting query string to the pre-cached resources.
+// This is to avoid loading these resources from the disk cache.
+const INITIAL_CACHED_RESOURCES_WITH_VERSIONS = INITIAL_CACHED_RESOURCES.map(path => {
+  return `${path}?v=${VERSION}`;
 });
 
-self.addEventListener('activate', function (event) {
-  event.waitUntil(
-      caches.keys().then(function(keys){
-          return Promise.all(keys.map(function(key, i){
-              if(key !== staticCacheName){
-                  return caches.delete(keys[i]);
-              }
-          }))
-      })
-  )
+// On install, fill the cache with all the resources we know we need.
+// Install happens when the app is used for the first time, or when a
+// new version of the SW is detected by the browser.
+// In the latter case, the old SW is kept around until the new one is
+// activated by a new client.
+self.addEventListener("install", event => {
+  self.skipWaiting();
+
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    cache.addAll(INITIAL_CACHED_RESOURCES_WITH_VERSIONS);
+  })());
+});
+
+// Activate happens after install, either when the app is used for the
+// first time, or when a new version of the SW was installed.
+// We use the activate event to delete old caches and avoid running out of space.
+self.addEventListener("activate", event => {
+  event.waitUntil((async () => {
+    const names = await caches.keys();
+    await Promise.all(names.map(name => {
+      if (name !== CACHE_NAME) {
+        return caches.delete(name);
+      }
+    }));
+    await clients.claim();
+  })());
+});
+
+// Main fetch handler.
+// A cache-first strategy is used, with a fallback to the network.
+// The static resources fetched here will not have the cache-busting query
+// string. So we need to add it to match the cache.
+self.addEventListener("fetch", event => {
+  // On fetch, go to the cache first, and then network.
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    const versionedUrl = `${event.request.url}?v=${VERSION}`;
+    const cachedResponse = await cache.match(versionedUrl);
+
+    if (cachedResponse) {
+      return cachedResponse;
+    } else {
+      const fetchResponse = await fetch(versionedUrl);
+      cache.put(versionedUrl, fetchResponse.clone());
+      return fetchResponse;
+    }
+  })());
 });
